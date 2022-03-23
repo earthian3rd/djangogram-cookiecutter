cc8f4d9ad54986cc8f354b0b5d0e02bd32ff4d23
earthian3rd <earthina3rd@gmail.com>
2022-03-23 17:28:07 +0900

    1st to lesson 3

--- djangogram/users/models.py
+++ djangogram/users/models.py
@@ -1,21 +1,31 @@
+from ast import BinOp
+import email
+from operator import mod
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField
+from django.db import models
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 
 
 class User(AbstractUser):
-    """
-    Default custom user model for djangogram.
-    If adding fields that need to be filled at user signup,
-    check forms.SignupForm and forms.SocialSignupForms accordingly.
-    """
+    GENDER_CHOICE = [
+        ('M', 'Male'),
+        ('F', 'Femail'),
+        ('C', 'Custom'),
+    ]
 
     #: First and last name do not cover name patterns around the globe
-    name = CharField(_("Name of User"), blank=True, max_length=255)
-    first_name = None  # type: ignore
-    last_name = None  # type: ignore
-
+    name = models.CharField(_("Name of User"), blank=True, max_length=255)
+    user_name = models.CharField(max_length=255, blank=True)
+    profile_photo = models.ImageField(blank=True)
+    website = models.URLField(max_length=255, blank=True)
+    bio = models.TextField(blank=True)
+    email  = models.CharField(max_length=255, blank=True)
+    phone_number = models.CharField(max_length=255, blank=True)
+    gender = models.CharField(max_length=255, blank=True, choices=GENDER_CHOICE)
+    followers = models.ManyToManyField("self")
+    following = models.ManyToManyField("self")
+    
     def get_absolute_url(self):
         """Get url for user's detail view.
 
