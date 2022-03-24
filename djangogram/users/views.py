b9e1f8218ecb25c0091615585fb2643b4fea4aa0
earthian3rd <earthina3rd@gmail.com>
2022-03-24 21:40:20 +0900

    lecture to 7

--- djangogram/users/views.py
+++ djangogram/users/views.py
@@ -1,7 +1,9 @@
+import imp
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
+from .forms import SignUpform
 
 
 def main(request):
@@ -18,3 +20,26 @@ def main(request):
             return HttpResponseRedirect(reverse('post:index'))
         else:
             return render(request, 'users/main.html')
+
+def signup(request):
+    if request.method == 'GET':
+        form = SignUpform()
+        
+        return render(request, 'users/signup.html', {'form': form})
+    elif request.method == 'POST':
+        form = SignUpform(request.POST)
+        
+        if form.is_valid():
+            form.save()
+            
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            
+            user = authenticate(request, username = username, password = password)
+            
+            if user is not None:
+                login(request, user)
+                return HttpResponseRedirect(reverse('post:index'))
+            
+        return render(request, 'users/main.html')
+        
